924d2441f23b08db13c2de1b97022a80f7a6c6b5
Azeez O. Odu <az6bcn@gmail.com>
2019-03-01 13:23:00 +0000

    Message, List and Member Components creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NotifierModule } from 'angular-notifier';
@@ -11,9 +12,18 @@ import { AuthService } from './Services/auth.service';
 import { HomeComponent } from './Home/Home.component';
 import { RegisterComponent } from './Register/Register.component';
 import { RouterModule } from '@angular/router';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListComponent } from './list/list.component';
+import { MessagesComponent } from './messages/messages.component';
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
-  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule, NotifierModule],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent, MemberListComponent, ListComponent, MessagesComponent],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule, NotifierModule, RouterModule.forRoot([
+    {path: '', component: HomeComponent},
+    {path: 'matches', component: MemberListComponent},
+    {path: 'lists', component: ListComponent},
+    {path: 'messages', component: MessagesComponent},
+    {path: '**', component: HomeComponent}
+  ])],
   providers: [AuthService],
   bootstrap: [AppComponent]
 })
